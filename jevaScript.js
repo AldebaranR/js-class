2af26f8ed992da734ef236e1a3916a8c2efd0258
AldebaranR <aldebaran1222@gmail.com>
2024-09-09 20:33:35 -0600

    repositorio1

---
+++ jevaScript.js
@@ -0,0 +1,32 @@
+var vestimenta = ['camisa','gorra','pantalon']
+
+function minifuncion(item,index){
+    console.log(index+''+item)
+}
+
+vestimenta.forEach(minifuncion)
+
+let array = Array.from('1A2B3C4D')
+console.log(array)
+
+//00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+let num = [32,33,15,40]
+mifuncion1()
+
+function validarArray(item){
+    return item>= 19
+}
+
+function mifuncion1 (){
+    console.log(num.every(validarArray))
+}
+
+
+let personas=[
+    {nombre: "Alan", edad: 53},
+    {nombre: "Abdiel", edad: 43},
+    {nombre: "aldebaran", edad: 33},
+    {nombre: "Uri", edad: 23},
+    {nombre: "Kike", edad: 13},
+]
